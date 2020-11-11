2402bdaf39f216d7ae3507e4a2638830e7c547eb
Ricardo <ricardogarciarreola@gmail.com>
2020-11-11 00:56:13 -0600

    Actualizacion

--- server/config/config.js
+++ server/config/config.js
@@ -6,9 +6,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
 let urlBD;
 
+// Local
 if ( process.env.NODE_ENV === 'dev' ) {
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
+    // Servidor
     urlBD = process.env.MONGO_URI;
 }
 
